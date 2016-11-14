11156b3ae37943325f24090ec0d03ed20c3341ce
danko <mudrinic016@gmail.com>
2016-11-14 09:41:47 +0100

    html button

--- app/src/main/java/com/example/dev/gitrepositories/details_activity_component/DetailsActivity.kt
+++ app/src/main/java/com/example/dev/gitrepositories/details_activity_component/DetailsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.dev.gitrepositories.details_activity_component
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import com.bumptech.glide.Glide
@@ -7,11 +9,14 @@ import com.example.dev.gitrepositories.R
 import com.example.dev.gitrepositories.models.Repository
 import com.example.dev.gitrepositories.modules.AppModule
 import com.hannesdorfmann.mosby.mvp.MvpActivity
+import com.mcxiaoke.koi.ext.onClick
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.activity_details_layout.*
 import kotlinx.android.synthetic.main.view_error.*
 import kotlinx.android.synthetic.main.view_loading.*
 import javax.inject.Inject
+
+
 
 class DetailsActivity
 @Inject
@@ -57,6 +62,14 @@ constructor() : MvpActivity<DetailsView, DetailsPresenter>(),  DetailsView {
         setContentView(R.layout.activity_details_layout)
         val repository: Repository = intent.extras.get("repository") as Repository
         setData(repository)
+
+        TVHtml.onClick {
+            val uri = Uri.parse(TVHtml.text.toString())
+            val intent = Intent()
+            intent.data = uri
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            applicationContext.startActivity(intent)
+        }
         showContent()
     }
 
